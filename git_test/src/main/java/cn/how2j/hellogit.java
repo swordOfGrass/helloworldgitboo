0620ffafde6141580d6ec770500a0f7857a28777
swordOfGrass <755926562@qq.com>
2022-03-11 20:36:54 +0800

    test

--- git_test/src/main/java/cn/how2j/hellogit.java
+++ git_test/src/main/java/cn/how2j/hellogit.java
@@ -2,6 +2,7 @@ package cn.how2j;
 
 public class hellogit {
 public static void main(String[] args) {
-	System.out.println("hello git");
+	System.out.println("hello git ");
+	int version = 1111;
 }
 }
